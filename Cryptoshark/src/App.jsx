1a50ffd6d92a20e98bb619bdd96425f66c416527
Eniola <eniolaadebanjo60@gmail.com>
2026-05-06 17:49:13 +0100

    fix: background color to footer

--- Cryptoshark/src/App.jsx
+++ Cryptoshark/src/App.jsx
@@ -7,6 +7,7 @@ import Plan from './pages/Plans'
 import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
 import Contact from './pages/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ Cryptoshark/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      © {new Date().getFullYear()} CryptoShark.
+    </div>
+  )
+}
+
+export default Footer
